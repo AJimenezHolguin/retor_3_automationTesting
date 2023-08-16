0f98dfb8bf1c5ce62776356849d53a5e016509ac
AndersonJimenez <anjimhol23@gmail.com>
2023-08-16 15:38:55 -0500

    se realiza prueba de testing a la pagina hotels by anderson donde se validan las siguientes pruebas: verificar que las cards carguen al inicio del cargue de la pagina, verificar que las cards carguen dependiendo del selector de precio seleccionado, verificar que el boton clear borre todos los filtros

--- cypress/e2e/testingReto3.cy.js
+++ cypress/e2e/testingReto3.cy.js
@@ -1,44 +1,59 @@
 describe("inicio de seción", () => {
-
-    beforeEach(()=>{
-        cy.visit("http://127.0.0.1:5501/hotels.html");  
-    })
-    it("debe verificar que al cargar la pagina se muestren todos los hoteles", () => {
-        cy.wait(500);
-        cy.get(".container-general_cards").should("be.visible").find(".card").should("have.length",18);
-    })
-    it("Debe mostrar todos los tipos de precios cada uno con sus respectivos hoteles", () =>{
-        cy.get(".filter-prices").select("$");
-        cy.get(".container-general_cards").should("be.visible").should(($cards)=>{
-            const cardCount = $cards.length;
-            expect(cardCount).to.be.greaterThan(0);    
-        })
-        cy.get("#filter-prices").select("$$");
-        cy.get(".container-general_cards").should("be.visible").should(($cards)=>{
-            const cardCount = $cards.length;
-            expect(cardCount).to.be.greaterThan(0);
-        })    
-        cy.get("#filter-prices").select("$$$");
-        cy.get(".container-general_cards").should("be.visible").should(($cards)=>{
-            const cardCount = $cards.length;
-            expect(cardCount).to.be.greaterThan(0);
-        })    
-        cy.get("#filter-prices").select("$$$$");
-        cy.get(".container-general_cards").should("be.visible").should(($cards)=>{
-            const cardCount = $cards.length;
-            expect(cardCount).to.be.greaterThan(0);
-        })    
-        cy.get("#filter-prices").select("All");
-        cy.get(".container-general_cards").should("be.visible").should(($cards)=>{
-            const cardCount = $cards.length;
-            expect(cardCount).to.be.greaterThan(0);
-        })    
-    })
-    it("Debe limpiar los filtros y verificar que se muestren nuevamente las 18 cards de hoteles", ()=>{ 
-        cy.get(".filter_countries").select("country3");
-        cy.get(".filter_countries").select("country2");
-        cy.get(".filter-prices").select("price3"); 
-        cy.get(".filter_clear").click();
-        cy.get(".container-general_cards").should("be.visible").find(".card").should("have.length",18);
-    })
-})
+  beforeEach(() => {
+    cy.visit("http://127.0.0.1:5501/hotels.html");
+  });
+  it("debe verificar que al cargar la pagina se muestren todos los hoteles", () => {
+    cy.wait(500);
+    cy.get(".container-general_cards")
+      .should("be.visible")
+      .find(".card")
+      .should("have.length", 18);
+  });
+  it("Debe mostrar todos los tipos de precios cada uno con sus respectivos hoteles", () => {
+    cy.get(".filter-prices").select("$");
+    cy.get(".container-general_cards")
+      .should("be.visible")
+      .should(($cards) => {
+        const cardCount = $cards.length;
+        expect(cardCount).to.be.greaterThan(0);
+      });
+    cy.get("#filter-prices").select("$$");
+    cy.get(".container-general_cards")
+      .should("be.visible")
+      .should(($cards) => {
+        const cardCount = $cards.length;
+        expect(cardCount).to.be.greaterThan(0);
+      });
+    cy.get("#filter-prices").select("$$$");
+    cy.get(".container-general_cards")
+      .should("be.visible")
+      .should(($cards) => {
+        const cardCount = $cards.length;
+        expect(cardCount).to.be.greaterThan(0);
+      });
+    cy.get("#filter-prices").select("$$$$");
+    cy.get(".container-general_cards")
+      .should("be.visible")
+      .should(($cards) => {
+        const cardCount = $cards.length;
+        expect(cardCount).to.be.greaterThan(0);
+      });
+    cy.get("#filter-prices").select("All");
+    cy.get(".container-general_cards")
+      .should("be.visible")
+      .should(($cards) => {
+        const cardCount = $cards.length;
+        expect(cardCount).to.be.greaterThan(0);
+      });
+  });
+  it("Debe limpiar los filtros y verificar que se muestren nuevamente las 18 cards de hoteles", () => {
+    cy.get(".filter_countries").select("country3");
+    cy.get(".filter_countries").select("country2");
+    cy.get(".filter-prices").select("price3");
+    cy.get(".filter_clear").click();
+    cy.get(".container-general_cards")
+      .should("be.visible")
+      .find(".card")
+      .should("have.length", 18);
+  });
+});
